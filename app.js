eac1ff92e83315e7bac766e5d051c62402ce3368
Thatoneguy051 <drianwessels@gmail.com>
2024-07-17 15:35:56 +0200

    Final stuff

--- app.js
+++ app.js
@@ -14,13 +14,21 @@ const reviewRoutes = require('./routes/reviews');
 const session = require('express-session');
 const flash = require('connect-flash');
 
+const MongoDBStore = require("connect-mongo")(session)
+
+const helmet = require('helmet')
+
+const mongoSanitize = require('express-mongo-sanitize');
+
 const userRoutes= require('./routes/users');
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const User = require('./models/user')
-
+// const dbUrl = process.env.DB_URL
+// 'mongodb://localhost:27017/yelp-camp'
+const dbUrl = 'mongodb://localhost:27017/yelp-camp'
 mongoose.set('strictQuery', true);
-mongoose.connect('mongodb://localhost:27017/yelp-camp');
+mongoose.connect(dbUrl);
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
@@ -35,18 +43,74 @@ app.engine('ejs', ejsMate)
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 app.use(express.static('public'))
+app.use(mongoSanitize())
+
+const store = new MongoDBStore({
+    url:dbUrl,
+    secret:'this should be a better secret',
+    touchAfter: 24 * 60 * 60
+});
+
+store.on("error", function(e){
+   console.log("session store error", e) 
+})
 const sessionConfig = {
+       store,
+       name: 'session',
        secret:'this should be a better secret',
        resave: false,
        saveUninitialized: true,
        cookie: {
             httpOnly: true,
+            // secure: true,
             expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
             maxAge: 1000 * 60 * 60 * 24 * 7
                }
 }
 app.use(session(sessionConfig))
 app.use(flash())
+app.use(helmet())
+
+const scriptSrcUrls = [
+    "https://stackpath.bootstrapcdn.com/",
+    "https://kit.fontawesome.com/",
+    "https://cdnjs.cloudflare.com/",
+    "https://cdn.jsdelivr.net",
+    "https://cdn.maptiler.com/", // add this
+];
+const styleSrcUrls = [
+    "https://kit-free.fontawesome.com/",
+    "https://stackpath.bootstrapcdn.com/",
+    "https://fonts.googleapis.com/",
+    "https://use.fontawesome.com/",
+    "https://cdn.jsdelivr.net",
+    "https://cdn.maptiler.com/", // add this
+];
+const connectSrcUrls = [
+    "https://api.maptiler.com/", // add this
+];
+const fontSrcUrls = [];
+app.use(
+    helmet.contentSecurityPolicy({
+        directives: {
+            defaultSrc: [],
+            connectSrc: ["'self'", ...connectSrcUrls],
+            scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+            styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+            workerSrc: ["'self'", "blob:"],
+            objectSrc: [],
+            imgSrc: [
+                "'self'",
+                "blob:",
+                "data:",
+                "https://res.cloudinary.com/dolkq3jxl/", 
+                "https://images.unsplash.com/",
+                "https://api.maptiler.com/",
+            ],
+            fontSrc: ["'self'", ...fontSrcUrls],
+        },
+    })
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -66,6 +130,10 @@ app.use((req, res, next) => {
 app.use('/', userRoutes)
 app.use('/campgrounds', campgroundRoutes);
 app.use('/campgrounds/:id/reviews', reviewRoutes)
+
+app.get('/', (req, res) => {
+    res.render('home')
+});
 
 app.all('*', (req, res, next) => {
     next(new ExpressError('Page Not Found', 404))
